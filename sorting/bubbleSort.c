51069efed748c6e841a7cba681306c709d8708b4
mogoo7zn <znwangking@hotmail.com>
2023-11-10 15:21:08 +0800

    first push bubbleSort

---
+++ sorting/bubbleSort.c
@@ -0,0 +1,41 @@
+//
+// Created by znwang on 2023/11/09.
+//
+
+#include "sorting_public _headfile.h"
+//#define SWAP(type, a, b) (type temp = (a), (a) = (b), (b) = temp)
+
+void swap(int *a, int *b);
+int main(void) {
+    int number, i, j;
+    scanf_s("%d", &number);
+    int value[number];
+    for(i = 0; i < number; i++)
+    {
+        scanf_s("%d", value + i);
+    }
+
+    for(i = 0; i < number; i++)
+    {
+        for(j = 0; j < number - 1; j++)
+        {
+            if(value[j] > value[j + 1])
+                swap(value + j, value + j + 1);
+//                SWAP(int, value[j], value[j + 1]);
+        }
+    }
+
+    for(i = 0; i < number; i++)
+    {
+        printf_s("%d ", value[i]);
+    }
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
---
+++ sorting/sorting_public _headfile.h
@@ -0,0 +1,11 @@
+//
+// Created by znwan on 2023/11/10.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifndef SORTING_SORTING_PUBLIC__HEADFILE_H
+#define SORTING_SORTING_PUBLIC__HEADFILE_H
+
+#endif //SORTING_SORTING_PUBLIC__HEADFILE_H
